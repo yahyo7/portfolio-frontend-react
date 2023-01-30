2fedc6c3c5c73085c3b4381b353c96024f4aa059
yaxyo7 <ahmadaliev.sd@gmail.com>
2023-01-30 23:31:39 +0900

    header component finished and about component started

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import { motion } from 'framer-motion'
+import {images} from '../../constants' 
 import './About.scss'
+
+const abouts = [
+  { title: "Web Development", description: "I am a good web developer", imgUrl: images.about01},
+  { title: "Web Design", description: "I am a good web designer", imgUrl: images.about02},
+  { title: "UI/UX", description: "I am a good UI/UX prof", imgUrl: images.about03}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>
+      I know that 
+      <span> Done </span>
+      is <br/> better than
+      <span> Not Doing.</span>
+    </h2>
+
+    <div className='app__profiles'>
+      {abouts.map((about, index) => (
+        <motion.div
+        whileInView={{opacity:1}}
+        whileHover={{scale: 1.1}}
+        transition={{duration: 0.5, type: 'tween'}}
+        className='app__profile-item'
+        key={about.title + index}
+        >
+          <img src={about.imgUrl} alt={about.title}/>
+          <h2 className='bold-text' style={{marginTop: 20}} >{about.title}</h2>
+          <h2 className='bold-text' style={{marginTop: 10}} >{about.description}</h2>
+        </motion.div>
+      ))}
+    </div>
+
+    </>
   )
 }
 
-export default About
+export default About 
