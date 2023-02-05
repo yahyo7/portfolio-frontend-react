dc5b287654899653129d15abff89c16579d00a70
yaxyo7 <ahmadaliev.sd@gmail.com>
2023-02-06 00:01:21 +0900

    abouts component updated

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,15 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import { motion } from 'framer-motion'
-import {images} from '../../constants' 
+// import {images} from '../../constants' 
 import './About.scss'
-
-const abouts = [
-  { title: "Web Development", description: "I am a good web developer", imgUrl: images.about01},
-  { title: "Web Design", description: "I am a good web designer", imgUrl: images.about02},
-  { title: "UI/UX", description: "I am a good UI/UX prof", imgUrl: images.about03}
-]
+import {urlFor,client} from '../../client'
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(()=> {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data)=> setAbouts(data))
+    }, [])
+
   return (
     <>
     <h2 className='head-text'>
@@ -28,7 +33,7 @@ const About = () => {
         className='app__profile-item'
         key={about.title + index}
         >
-          <img src={about.imgUrl} alt={about.title}/>
+          <img src={urlFor(about.imgUrl)} alt={about.title}/>
           <h2 className='bold-text' style={{marginTop: 20}} >{about.title}</h2>
           <h2 className='bold-text' style={{marginTop: 10}} >{about.description}</h2>
         </motion.div>
